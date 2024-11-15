8bddb2885d7e38e4e2a44623fb183d38fb6a0b62
lionelfr23 <jp7523090@gmail.com>
2024-11-15 17:50:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Animación al hacer scroll
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+// Scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+        });
+    });
+});
